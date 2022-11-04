998308f503d5a961a0f19daa39a38221e7d8c897
Facug03 <facundogonzales1550@gmail.com>
2022-11-04 15:23:37 -0300

    redirect if no match

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ function App () {
       <Routes>
         <Route path='/' element={<Main />} />
         <Route path='/create' element={<Create />} />
+        <Route path='*' element={<Main />} />
       </Routes>
       <Footer />
     </>
